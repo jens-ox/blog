beda15856f967c8f1c473dde79b2fc83a08b7a92
Jens Ochsenmeier <github@jens-ox.de>
2023-01-08 11:50:02 +0100

    clean up search component

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -61,7 +61,7 @@ export default function Blog() {
               type="text"
               onChange={(e) => setSearchValue(e.target.value)}
               placeholder="Search posts"
-              className="block w-full px-4 py-2 text-gray-900 bg-white border border-gray-300 rounded-md dark:border-gray-900 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-800 dark:text-gray-100"
+              className="block w-full px-4 py-2 rounded-lg dark:bg-white/5 bg-black/5 focus:ring-1 dark:focus:ring-white/50 focus:ring-black/50 focus:outline-none"
             />
             <svg
               className="absolute w-5 h-5 text-gray-400 right-3 top-3 dark:text-gray-300"
