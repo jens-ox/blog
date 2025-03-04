2b2944002b01ea0fe37bd78ed615ad6ffb7cc23d
Jens Ochsenmeier <github@jens-ox.de>
2025-03-04 18:09:06 +0100

    revamp

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Atkinson_Hyperlegible } from 'next/font/google'
 import './globals.css'
 import { clsx } from 'clsx'
 import { EnvelopeClosedIcon, GitHubLogoIcon, LinkedInLogoIcon } from '@radix-ui/react-icons'
+import Image from 'next/image'
+import { Contexts } from '@/components/Contexts'
 import { Navigation } from '@/components/Navigation'
 
 export const metadata: Metadata = {
@@ -27,49 +29,70 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={clsx(sans.variable, 'font-sans min-h-screen')}>
-        <main className="w-full max-w-7xl mx-auto py-6 lg:py-12 px-6 grid grid-cols-1 lg:grid-cols-3 gap-12">
-          <div className="flex flex-col gap-5 pt-24">
-            <div className="prose dark:prose-invert">
-              <h1>Hi!</h1>
-              <p>
-                I&apos;m Jens Ochsenmeier. I (enable teams to) build high-quality fullstack
-                applications that turn machine learning models into actionable insights and
-                decisions.
-              </p>
+        <Contexts>
+          <main className="w-full max-w-7xl mx-auto px-6 grid grid-cols-1 lg:grid-cols-3 gap-12">
+            <div className="pt-12 lg:pt-0">
+              <div className="flex flex-col md:flex-row lg:flex-col print:flex-row gap-8 sticky top-6 lg:top-12">
+                <div className="max-w-[370px] mx-auto lg:max-w-none">
+                  <Image
+                    className="rounded-lg p-1 bg-white dark:bg-white/5 border border-gray-200 dark:border-gray-200/5 shadow dark:shadow-none"
+                    src="/me.jpg"
+                    width={1000}
+                    height={1000}
+                    alt="A picture of me"
+                  />
+                </div>
+                <div className="flex flex-col gap-8">
+                  <div className="prose dark:prose-invert print:prose-sm">
+                    <h2>Hi!</h2>
+                    <p>
+                      I&apos;m Jens Ochsenmeier. I lead the Fullstack Engineering Team at{' '}
+                      <a href="https://quantco.com" target="_blank" rel="noreferrer">
+                        QuantCo
+                      </a>
+                      .
+                    </p>
+                    <p>
+                      I (enable teams to) build high-quality fullstack applications that turn
+                      machine learning models into actionable insights and decisions.
+                    </p>
+                  </div>
+                  <div className="flex flex-row lg:flex-col flex-wrap gap-3 print:hidden">
+                    <div>
+                      <a
+                        href="https://github.com/jens-ox"
+                        target="_blank"
+                        className="button"
+                        rel="noreferrer"
+                      >
+                        <GitHubLogoIcon /> <span>GitHub</span>
+                      </a>
+                    </div>
+                    <div>
+                      <a
+                        href="https://www.linkedin.com/in/jens-ox/"
+                        target="_blank"
+                        className="button"
+                        rel="noreferrer"
+                      >
+                        <LinkedInLogoIcon /> <span>LinkedIn</span>
+                      </a>
+                    </div>
+                    <div>
+                      <a href="mailto:hi@jens-ox.de" className="button">
+                        <EnvelopeClosedIcon /> <span>Mail</span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="flex flex-row lg:flex-col flex-wrap gap-3">
-              <div>
-                <a
-                  href="https://github.com/jens-ox"
-                  target="_blank"
-                  className="button"
-                  rel="noreferrer"
-                >
-                  <GitHubLogoIcon /> <span>GitHub</span>
-                </a>
-              </div>
-              <div>
-                <a
-                  href="https://www.linkedin.com/in/jens-ox/"
-                  target="_blank"
-                  className="button"
-                  rel="noreferrer"
-                >
-                  <LinkedInLogoIcon /> <span>LinkedIn</span>
-                </a>
-              </div>
-              <div>
-                <a href="mailto:hi@jens-ox.de" className="button">
-                  <EnvelopeClosedIcon /> <span>Mail</span>
-                </a>
-              </div>
+            <div className="col-span-2 flex flex-col gap-12 pb-12">
+              <Navigation />
+              <div>{children}</div>
             </div>
-          </div>
-          <div className="col-span-2 flex flex-col gap-12">
-            <Navigation />
-            <div>{children}</div>
-          </div>
-        </main>
+          </main>
+        </Contexts>
       </body>
     </html>
   )
--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -3,17 +3,23 @@
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
+const links = [
+  { href: '/', name: 'Resume' },
+  { href: '/projects', name: 'Projects' }
+]
+
 export const Navigation = () => {
   const pathname = usePathname()
 
   return (
-    <div className="flex gap-2 bg-white dark:bg-gray-100/10 rounded-lg p-1 border border-gray-200 dark:border-gray-100/50">
-      <Link href="/" className="nav-link" data-active={pathname === '/'}>
-        Resume
-      </Link>
-      <Link href="/projects" className="nav-link" data-active={pathname === '/projects'}>
-        Projects
-      </Link>
+    <div className="sticky pt-6 lg:pt-12 top-0 bg-gray-50 dark:bg-gray-800 print:hidden">
+      <div className="flex gap-2 bg-white dark:bg-white/5 rounded-lg p-1 border border-gray-200 dark:border-gray-200/5 shadow-sm">
+        {links.map((l) => (
+          <Link key={l.href} href={l.href} className="nav-link" data-active={pathname === l.href}>
+            {l.name}
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
